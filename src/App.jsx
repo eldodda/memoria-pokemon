aad9265322da657450f0996b0746809101e16037
Douglas Lima <dugbits@gmail.com>
2026-03-06 13:52:15 -0300

    feat: :sparkles: add: menu de configurações, opções de imagem (sprite ou arte), opções de som (anime ou game) e modo noturno.

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,50 @@ import { useGame } from "./hooks/useGame.js";
 import './App.css';
 
 function App() {
-    const { cartas, prepFase, faseAtual, trataEscolha, choice1, choice2, recorde } = useGame();
+    const { cartas, prepFase, faseAtual, trataEscolha, choice1, choice2, recorde, config, setConfig } = useGame();
+    const [menuAberto, setMenuAberto] = useState(false);
 
     return (
-        <div className="App">
+        <div className={`App ${config.darkMode ? 'night-mode' : ''}`}>
             <header className="header-jogo">
                 <div className="titulo-container">
                     <img src="/titulo.png" alt="Pokemoria" className="img-titulo" />
+                    <button className="btn-config" onClick={() => setMenuAberto(!menuAberto)}>
+                        <img src="/sets.png" alt="Config" />
+                    </button>
                     <button className="btn-refresh" onClick={() => prepFase(true)}>
-                        <img src="/refresh.png" alt="Reseta o jogo" />
+                        <img src="/refresh.png" alt="Reset" />
                     </button>
                 </div>
                 <div className="recorde-badge">
                     Nível: {faseAtual} | 🏆 Recorde: {recorde}
                 </div>
             </header>
+
+            {menuAberto && (
+                <div className="menu-config">
+                    <h3>Configurações</h3>
+
+                    <div className="opcao">
+                        <span>Visual:</span>
+                        <button onClick={() => setConfig({ ...config, useSprites: false })}>Art</button>
+                        <button onClick={() => setConfig({ ...config, useSprites: true })}>GBA</button>
+                    </div>
+
+                    <div className="opcao">
+                        <span>Sons:</span>
+                        <button onClick={() => setConfig({ ...config, useAnimeCries: false })}>GBA</button>
+                        <button onClick={() => setConfig({ ...config, useAnimeCries: true })}>Anime</button>
+                    </div>
+
+                    <button className="btn-drk-mode" onClick={() => setConfig({...config, darkMode: config.darkMode ? false : true})}>
+                        <img src={config.darkMode ? "/sun.png" : "/moon.png"} alt="Night Mode" />
+                    </button>
+
+                    <button className="btn-fechar" onClick={() => setMenuAberto(false)}>Voltar</button>
+                </div>
+            )}
+
             <div className="card-grid">
                 {cartas.map(carta => {
                     const isFlipped = carta === choice1 || carta === choice2 || carta.combinado;
--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -1,204 +1,152 @@
 import { useEffect, useState } from "react";
 import { Haptics, ImpactStyle } from "@capacitor/haptics"; // Recursos do Capacitor para vibração.
 
-const cartasIniciais = [{       //  Array das cartas com suas imagens e propriedades para controle do jogo.
-    src: '/assets/arts/1.png',
-    cry: '/assets/cries/1.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/4.png',
-    cry: '/assets/cries/4.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/7.png',
-    cry: '/assets/cries/7.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/10.png',
-    cry: '/assets/cries/10.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/13.png',
-    cry: '/assets/cries/13.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/19.png',
-    cry: '/assets/cries/19.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/23.png',
-    cry: '/assets/cries/23.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/25.png',
-    cry: '/assets/cries/25.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/27.png',
-    cry: '/assets/cries/27.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/29.png',
-    cry: '/assets/cries/29.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/32.png',
-    cry: '/assets/cries/32.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/39.png',
-    cry: '/assets/cries/39.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/41.png',
-    cry: '/assets/cries/41.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/43.png',
-    cry: '/assets/cries/43.ogg',
-    combinado: false
-},
-{
-    src: '/assets/arts/46.png',
-    cry: '/assets/cries/46.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/48.png',
-    cry: '/assets/cries/48.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/50.png',
-    cry: '/assets/cries/50.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/52.png',
-    cry: '/assets/cries/52.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/54.png',
-    cry: '/assets/cries/54.ogg',
-    combinado: false
-}, {
-    src: '/assets/arts/56.png',
-    cry: '/assets/cries/56.ogg',
-    combinado: false
-},
-]
-
 export const useGame = () => {      // Motor lógico do jogo.
-    const [disabled, setDisabled] = useState(false);
-    const [cartas, setCartas] = useState([]);
-    const [choice1, setChoice1] = useState(null);
-    const [choice2, setChoice2] = useState(null);
-    const [faseAtual, setFaseAtual] = useState(1);
-    const [recorde, setRecorde] = useState(() => {
-        const salvo = localStorage.getItem('memoria_recorde');
-        return salvo ? parseInt(salvo) : 0;
-    });
+	const [disabled, setDisabled] = useState(false);
+	const [cartas, setCartas] = useState([]);
+	const [choice1, setChoice1] = useState(null);
+	const [choice2, setChoice2] = useState(null);
+	const [faseAtual, setFaseAtual] = useState(1);
+	const [recorde, setRecorde] = useState(() => {
+		const salvo = localStorage.getItem("recorde-salvo");
+		return salvo ? parseInt(salvo) : 0;
+	});
+	const [config, setConfig] = useState(() => {
+		const configSalva = localStorage.getItem("config-salva");
+		return configSalva ? JSON.parse(configSalva) : {
+			useSprites: false,
+			useAnimeCries: false,
+			nightMode: false,
+		};
+	});
 
-    const maxPares = 9; // Máximo de cartas na tela (9 pares = 18 cartas);
+	const totalPokemon = 151;
+	const cartasIniciais = Array.from({ length: totalPokemon }, (_, i) => {
+		const id = i + 1;			//	cartasIniciais agora cria um array puxando os arquivos diretamente da pasta,
+		return {					// me poupando de criar um array quilométrico para trazer os pokémon.
+			id_pokemon: id,
+			src: config.useSprites ? `/assets/sprites/${id}.png` : `/assets/arts/${id}.png`,
+			cry: config.useAnimeCries ? `/assets/animeCries/${id}.ogg` : `/assets/cries/${id}.ogg`,
+			combinado: false
+		};
+	});
 
-    const prepFase = (resetTotal = false) => {  // Prepara a fase.
-        if (resetTotal) {
-            setFaseAtual(1);
-            return;
-        }
+	useEffect(() => {		//	Mantém o jogo atualizado com as configs atuais.
+		setCartas(prevCartas =>
+			prevCartas.map(carta => ({
+				...carta,
+				src: config.useSprites
+					? `/assets/sprites/${carta.id_pokemon}.png`
+					: `/assets/arts/${carta.id_pokemon}.png`,
+				cry: config.useAnimeCries
+					? `/assets/animeCries/${carta.id_pokemon}.ogg`
+					: `/assets/cries/${carta.id_pokemon}.ogg`
+			})),
+		);
+		setChoice1(null);
+		setChoice2(null);
+		localStorage.setItem("config-salva", JSON.stringify(config));
+	}, [config]);
 
-        const quantPares = faseAtual <= 8 ? faseAtual + 1 : maxPares;   // Controla o progresso das fases.
-        const cartasSorteadas = [...cartasIniciais]                     // Separa cartas aleatórias para cada fase, diminuindo a repetição:
-            .sort(() => Math.random() - 0.5)                            // Embaralha;
-            .slice(0, quantPares);                                      // Separa a quantidade para a fase.
+	const maxPares = 9; // Máximo de cartas na tela (9 pares = 18 cartas);
 
-        const shuffled = [...cartasSorteadas, ...cartasSorteadas]        // Gera os pares e os prepara para a fase:
-            .sort(() => Math.random() - 0.5)                             // Embaralha;
-            .map((carta) => { return { ...carta, id: Math.random(), combinado: false } });  // Seta todas as cartas como 'não combinadas' (combinado: false).
+	const prepFase = (resetTotal = false) => {  // Prepara a fase.
+		if (resetTotal) {
+			setFaseAtual(1);
+			return;
+		}
 
-        setCartas(shuffled);    // Prepara as cartas da fase;
-        setChoice1(null);       // Limpa a primeira escolha
-        setChoice2(null);       // e a segunda;
-    }
+		const quantPares = faseAtual <= 8 ? faseAtual + 1 : maxPares;   // Controla o progresso das fases.
+		const cartasSorteadas = [...cartasIniciais]                     // Separa cartas aleatórias para cada fase, diminuindo a repetição:
+			.sort(() => Math.random() - 0.5)                            // Embaralha;
+			.slice(0, quantPares);                                      // Separa a quantidade para a fase.
 
-    const trataEscolha = (carta) => {
-        if (!disabled && carta.id !== choice1?.id && !carta.combinado) {
-            choice1 ? setChoice2(carta) : setChoice1(carta);
-        }
-    }
+		const shuffled = [...cartasSorteadas, ...cartasSorteadas]        // Gera os pares e os prepara para a fase:
+			.sort(() => Math.random() - 0.5)                             // Embaralha;
+			.map((carta) => { return { ...carta, id: Math.random(), combinado: false }; });  // Seta todas as cartas como 'não combinadas' (combinado: false).
 
-    const resetTurno = () => {  // Função para resetar o turno:
-        setChoice1(null);           // Zera a escolha um
-        setChoice2(null);           // e a dois;
-        setDisabled(false);         // Destrava as cartas para novas escolhas.
-    }
+		setCartas(shuffled);    // Prepara as cartas da fase;
+		setChoice1(null);       // Limpa a primeira escolha
+		setChoice2(null);       // e a segunda;
+	};
 
-    const playSom = (caminhoAudio, volume) => { // Função para tocar os sons de acerto, erro e mudança de fase.
-        if (!caminhoAudio) return;  // Se o caminhoAudio não for fornecido, segue sem mesmo;
-        const audio = new Audio(caminhoAudio);
-        audio.volume = volume;
-        audio.play();
-    }
+	const trataEscolha = (carta) => {
+		if (!disabled && carta.id !== choice1?.id && !carta.combinado) {
+			choice1 ? setChoice2(carta) : setChoice1(carta);
+		}
+	};
 
-    useEffect(() => {
-        if (choice1 && choice2) {   // Se as duas escolhas já foram feitas,
-            setDisabled(true);      // trava as cartas, impedindo novas escolhas.
-            if (choice1.src === choice2.src) {                  // Se as escolhas forem iguais,
-                playSom(choice1.cry, 1);                // toca o som de acerto e
-                setCartas(escolhidas => {                       // pega as cartas escolhidas e
-                    return escolhidas.map(carta => {            // mapeia cada carta:
-                        if (carta.src === choice1.src) {            // Mais uma comparação para o .map(), e se confirmar,
-                            return { ...carta, combinado: true }   // registra que essas cartas já foram combinadas.
-                        } else {            // Senão,
-                            return carta;   // voltamos as cartas ao normal.
-                        }
-                    });
-                });
-                resetTurno();   // E resetamos o turno.
-            } else {                                            // Se as cartas não forem iguais (erro):
-                playSom('/err.mp3', 1),                         // Toca o som de erro.
-                    setTimeout(() => {
-                        resetTurno();                                   // Reseta o turno
-                        Haptics.impact({ style: ImpactStyle.Light })   // e vibra.
-                    },
-                        1000
-                    );
-            }
-        }
-    }, [choice1, choice2]); // As variáveis a serem vigiadas pelo useEffect.
+	const resetTurno = () => {  // Função para resetar o turno:
+		setChoice1(null);           // Zera a escolha um
+		setChoice2(null);           // e a dois;
+		setDisabled(false);         // Destrava as cartas para novas escolhas.
+	};
 
-    useEffect(() => {
-        const ganhou = cartas.length > 0 && cartas.every(carta => carta.combinado); // Condição de vitória: todas as cartas combinadas (combinado: true).
-        if (ganhou) {   // Se venceu:
-            setTimeout(() => {
-                setFaseAtual(prev => {          // Preparamos uma nova fase
-                    const novaFase = prev + 1;  // que é a fase atual + 1.
-                    if (novaFase > recorde) {                                   // Se a nova fase for maior que o recorde atual:
-                        setRecorde(novaFase);                                           // Ela passa a ser o novo recorde.
-                        localStorage.setItem('memoria_recorde', novaFase.toString());   // E o recorde é guardado no localStorage.
-                    }
-                    return novaFase;
-                });
-                playSom('/lvl.mp3', 1);   // E tocamos o som de vitória.
-            }, 700);
-        }
-    }, [cartas]);
+	const playSom = (caminhoAudio, volume) => { // Função para tocar os sons de acerto, erro e mudança de fase.
+		if (!caminhoAudio) return;  // Se o caminhoAudio não for fornecido, segue sem mesmo;
+		const atualiza = `${caminhoAudio}?v=${new Date().getTime()}`;
+		const audio = new Audio(atualiza);
+		audio.volume = volume;
+		audio.play();
+	};
 
-    useEffect(() => {
-        prepFase(); // Sempre que faseAtual for atualizada, prepara a fase.
-    }, [faseAtual]);
+	useEffect(() => {
+		if (choice1 && choice2) {   // Se as duas escolhas já foram feitas,
+			setDisabled(true);      // trava as cartas, impedindo novas escolhas.
+			if (choice1.src === choice2.src) {                  // Se as escolhas forem iguais,
+				playSom(choice1.cry, 1);                // toca o som de acerto e
+				setCartas(escolhidas => {                       // pega as cartas escolhidas e
+					return escolhidas.map(carta => {            // mapeia cada carta:
+						if (carta.src === choice1.src) {            // Mais uma comparação para o .map(), e se confirmar,
+							return { ...carta, combinado: true };   // registra que essas cartas já foram combinadas.
+						} else {            // Senão,
+							return carta;   // voltamos as cartas ao normal.
+						}
+					});
+				});
+				resetTurno();   // E resetamos o turno.
+			} else {                                            // Se as cartas não forem iguais (erro):
+				playSom("/err.mp3", 1),                         // Toca o som de erro.
+				setTimeout(() => {
+					resetTurno();                                   // Reseta o turno
+					Haptics.impact({ style: ImpactStyle.Light });   // e vibra.
+				},
+				1000
+				);
+			}
+		}
+	}, [choice1, choice2]); // As variáveis a serem vigiadas pelo useEffect.
 
-    return {
-        cartas,
-        prepFase,
-        faseAtual,
-        trataEscolha,
-        choice1,
-        choice2,
-        recorde,
-    }
-}
+	useEffect(() => {
+		const ganhou = cartas.length > 0 && cartas.every(carta => carta.combinado); // Condição de vitória: todas as cartas combinadas (combinado: true).
+		if (ganhou) {   // Se venceu:
+			setTimeout(() => {
+				setFaseAtual(prev => {          // Preparamos uma nova fase
+					const novaFase = prev + 1;  // que é a fase atual + 1.
+					if (novaFase > recorde) {                                   // Se a nova fase for maior que o recorde atual:
+						setRecorde(novaFase);                                           // Ela passa a ser o novo recorde.
+						localStorage.setItem("recorde-salvo", novaFase.toString());   // E o recorde é guardado no localStorage.
+					}
+					return novaFase;
+				});
+				playSom("/lvl.mp3", 1);   // E tocamos o som de vitória.
+			}, 700);
+		}
+	}, [cartas]);
+
+	useEffect(() => {
+		prepFase(); // Sempre que faseAtual for atualizada, prepara a fase.
+	}, [faseAtual]);
+
+	return {
+		cartas,
+		prepFase,
+		faseAtual,
+		trataEscolha,
+		choice1,
+		choice2,
+		recorde,
+		config,
+		setConfig,
+	};
+};
